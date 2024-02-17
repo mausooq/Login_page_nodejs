9054b8f693f71a95b2abc2b02876bc213bbcb5f2
mausooq <mauchoomausooq051@gmail.com>
2024-02-17 09:59:28 +0530

    frontend form

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express =require('express')
-
+const bcrypt = require('bcrypt')
 const app = express();
 app.set('view-engin','ejs')
 app.get('/',(req,res) =>{
     res.render( 'index.ejs',{name:"mausooq"});
 })
 app.get('/login',(req,res) =>{
-    res.render('login.ejx')
+    res.render('login.ejs')
 })
 app.get('/register',(req,res) =>{
-    res.render('register.ejx')
+    res.render('register.ejs')
 })
+app.post('/register',async (req,res)=>{
 
+})
 app.listen(8080,() => {
     console.log("Server is running on port 8080");
 })
