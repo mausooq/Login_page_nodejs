6e56345b38bf35465c59468a6442e3abf7224838
mausooq <mauchoomausooq051@gmail.com>
2024-02-17 20:41:18 +0530

    done

--- app.js
+++ app.js
@@ -1,49 +1,91 @@
-const express =require('express')
-const bcrypt = require('bcrypt')
-const app = express();
-const passport = require("passport");
-const flash = require('express-flash')
-const session = require('express-session')
-
-var bodyParser = require("body-parser")
-app.use(bodyParser.json())
-
-const initializePassport = require('./passport-config')
-initializePassport(passport,email => users.find(user =>user.email === email))
-
-const users =[];
-
-app.use(express.urlencoded({ extended: true })); // middleware for parsing application/x-www-form-url
-app.use(flash())
-app.use(session({
-    secert:process.env.SESSION_SECERT
-}))
-
-app.set('view-engin','ejs')
-app.get('/',(req,res) =>{
-    res.render( 'index.ejs',{name:"mausooq"});
-})
-app.get('/login',(req,res) =>{
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+  
+  const express = require('express')
+  const app = express()
+  const bcrypt = require('bcrypt')
+  const passport = require('passport')
+  const flash = require('express-flash')
+  const session = require('express-session')
+  const methodOverride = require('method-override')
+  
+  const initializePassport = require('./passport-config')
+  initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+  )
+  
+  const users = []
+  
+  app.set('view-engine', 'ejs')
+  app.use(express.urlencoded({ extended: false }))
+  app.use(flash())
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+  app.use(methodOverride('_method'))
+  
+  app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
+  })
+  
+  app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render('login.ejs')
-})
-app.get('/register',(req,res) =>{
+  })
+  
+  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }))
+  
+  app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render('register.ejs')
-})
-app.post('/register',async (req,res)=>{
+  })
+  
+  app.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
-        const hasedPaassword =await bcrypt.hash(req.body.password , 10);// hash the password using bcrypt    
-        users.push({
-            id:Date.now().toString(),
-            name:req.body.name,
-            email: req.body.email,
-            password :hasedPaassword
-        });
-        res.redirect("/login");
-    } catch  {
-        res.redirect('/register')
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      })
+      res.redirect('/login')
+    } catch {
+      res.redirect('/register')
     }
-    console.log(users)
-})
-app.listen(8080,() => {
-    console.log("Server is running on port 8080");
-})
+  })
+  
+  app.delete('/logout', (req, res) => {
+    req.logout(err => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/login');
+    });
+  });
+  
+  
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/login')
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+  }
+  
+  app.listen(3000)
--- passport-config.js
+++ passport-config.js
@@ -1,35 +1,29 @@
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
 
-function initialize(passport, getUserByEmail) {
-    const authenticateUser = async (email, password, done) => {
-        const user = getUserByEmail(email);
-        if (user == null) {
-            return done(null, false, { message: "Email not found" });
-        }
-        try {
-            if (await bcrypt.compare(password, user.password)) {
-                return done(null, user);
-            } else {
-                return done(null, false, { message: 'Password is incorrect' });
-            }
-        } catch (e) {
-            return done(e);
-        }
-    };
+function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
+    const user = getUserByEmail(email)
+    if (user == null) {
+      return done(null, false, { message: 'No user with that email' })
+    }
 
-    passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user)
+      } else {
+        return done(null, false, { message: 'Password incorrect' })
+      }
+    } catch (e) {
+      return done(e)
+    }
+  }
 
-    passport.serializeUser((user, done) => {
-        done(null, user.id);
-    });
-
-    passport.deserializeUser((id, done) => {
-        // Implement logic to fetch user by id and pass it to done callback
-        // Example: User.findById(id, function (err, user) {
-        //    done(err, user);
-        // });
-    });
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
+  passport.serializeUser((user, done) => done(null, user.id))
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+  })
 }
 
-module.exports = initialize;
+module.exports = initialize
